28eaa1c751ee6fa631554c6f4297ee9963a7d7a8
Nandini Proothi <46650119+nandiniproothi@users.noreply.github.com>
2021-01-21 17:07:01 +0530

    v0.1

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # unity-clock
 A basic clock app for iOS, PC, Mac made with Unity
+
+## Build Instructions
+
+- Create a Unity Project (install modules for iOS, Mac, Windows)
+- Add an empty GameObject called "test"
+- Add a UI Text object named "Text"
+- Add an Image object to the Canvas (This will cause your background to be white. Play around, change the colour or add an image)
+- Go to the Text object and add a component. Click on "New Script" and copy the contents of `clockScript.cs`
+- Head over to `File>Build Settings` and `Build and Run` for the selected platform.
---
+++ clockScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class clockScript : MonoBehaviour
+{   
+    private Text textClock; 
+
+    void Awake (){ 
+        textClock = GetComponent<Text>(); //make sure you include "UnityEngine.UI" or just "UnityEngine.UI.Text"
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hello world!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    DateTime time = DateTime.Now; //make sure you include "System" 
+    string hour = padding(time.Hour);
+    string minute = padding(time.Minute);
+    string second = padding(time.Second);
+    textClock.text = hour + ":" + minute + ":" + second;
+    }
+    string padding (int n){
+     return n.ToString().PadLeft(2, '0');
+  }
+}
+
